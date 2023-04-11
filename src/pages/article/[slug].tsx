75abe4ca48820fd88b46dee0b85e89baec67b0f4
Poramee Suriyajanno <pookoak8501@gmail.com>
2023-04-11 18:06:52 +0700

    update article and slug

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -1,10 +1,44 @@
 export default function article() {
     return (
       <div>
-        <div className="flex min-h-screen flex-col items-center justify-between p-24" style={{ fontFamily: 'Prompt' }}>
-          <div className="">
-              <p>Slugจ้า</p>
-          </div>
+        <div className="flex min-h-screen flex-col items-center justify-between " style={{ fontFamily: 'Prompt' }}>
+            <div className="z-0 flex absolute w-full bg-orange-50" style={{width:"100%", height:"600px"}}>
+              <div className="absolute right-0" style={{marginTop:"33.18px", marginRight:"96.43px"}}>
+                <svg width="608" height="567" viewBox="0 0 608 567" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g opacity="0.05">
+                <path d="M318.677 178.83V250.776C283.446 221.412 246.99 206.717 209.307 206.717C167.796 206.717 132.815 221.587 104.366 251.302C75.7658 280.841 61.4534 316.99 61.4534 359.747C61.4534 402.505 75.7658 437.602 104.366 466.64C132.966 495.679 168.046 510.174 209.557 510.174C231.026 510.174 249.242 506.694 264.255 499.735C272.637 496.255 281.294 491.574 290.302 485.666C299.285 479.758 308.743 472.624 318.702 464.287V537.536C283.822 557.187 247.19 567 208.807 567C151.056 567 101.788 546.923 60.978 506.769C20.3176 466.265 0 417.45 0 360.248C0 308.979 17.0148 263.268 51.0193 223.114C92.8807 173.923 147.053 149.34 213.511 149.34C250.903 149.34 284.848 159.153 318.702 178.805L318.677 178.83Z" fill="url(#paint0_linear_53_1355)"/>
+                <path d="M318.725 372.723V0.176025H397.1C434.673 0.176025 464.368 3.88383 486.14 11.2764C509.516 18.5067 530.73 30.8122 549.76 48.1927C588.309 83.2551 607.571 129.348 607.571 186.449C607.571 243.55 587.495 290.038 547.32 325.424C527.152 343.129 506.03 355.435 483.933 362.341C463.276 369.27 434.022 372.723 396.124 372.723H318.702H318.725ZM375.096 319.886H400.492C425.82 319.886 446.871 317.221 463.648 311.914C480.424 306.283 495.574 297.361 509.12 285.125C536.864 259.865 550.736 226.981 550.736 186.449C550.736 145.918 537.027 112.454 509.608 87.0325C484.932 64.345 448.568 53.0129 400.492 53.0129H375.096V319.863V319.886Z" fill="url(#paint1_linear_53_1355)"/>
+                </g>
+                <defs>
+                <linearGradient id="paint0_linear_53_1355" x1="159.351" y1="149.34" x2="159.351" y2="567" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#171717"/>
+                <stop offset="1" stop-color="#737373"/>
+                </linearGradient>
+                <linearGradient id="paint1_linear_53_1355" x1="463.137" y1="0.176025" x2="319.225" y2="373.261" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF971A"/>
+                <stop offset="1" stop-color="#FCD34D"/>
+                </linearGradient>
+                </defs>
+                </svg>
+              </div>
+            </div>
+            <div className="z-10 " style={{width:"822px"}}>
+              <div>
+                <div className="flex mt-16">
+                  <p>Tawatch Petkaew</p>
+                  <p className="ml-4">5 เมษายน 66</p>
+                </div>
+                <div className="text-5xl font-medium mt-6" style={{lineHeight:"57.6px"}}>
+                  <p>สูตรลัดยิงโฆษณาให้ปัง ฉบับมือใหม่ก็ทำได้ </p>
+                </div>
+                <div className="flex mt-6">
+                  <p>หมวดหมู่ : </p>
+                  <p className="ml-4">การตลาด</p>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div>
         </div>
       </div>
     )
